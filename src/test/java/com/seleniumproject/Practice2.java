f0a65b1cdd61b5397d26fee332ddf563977288af
rudresh <rudresh@LAPTOP-BJQHU9G0>
2022-05-31 12:23:54 +0530

    new project

---
+++ src/test/java/com/seleniumproject/Practice2.java
@@ -0,0 +1,23 @@
+package com.seleniumproject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice2 {
+
+	@Test
+	public void practice()
+	{
+	
+Reporter.log("practice", true);
+	}
+	@Test
+	public void practice1()
+	{
+		Reporter.log("practice1", true);
+	}
+	public void practice2()
+	{
+		Reporter.log("practice2", true);
+	}
+}
